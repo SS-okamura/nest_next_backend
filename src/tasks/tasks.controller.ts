4ccebc9e88d605f03646f8df055c13c4a07484b7
SS-okamura <ss.okamura0509@gmail.com>
2024-01-05 15:35:15 +0900

    feat: add tasks controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskInput } from './VO/CreateTaskInput';
+import { UpdateTaskInput } from './VO/UpdateTaskInput';
+
+@Controller('task')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  /**
+   * 全件取得	http://localhost:3000/task	GET
+   * 1件追加	http://localhost:3000/task	POST
+   * 1件更新	http://localhost:3000/task	PUT
+   * 1件削除	http://localhost:3000/task/:id	DELETE
+   */
+
+  @Get()
+  readAllTasks() {
+    return this.tasksService.readAllTasks();
+  }
+
+  @Post()
+  async createTodo(@Body() input: CreateTaskInput) {
+    return this.tasksService.createTask(input);
+  }
+
+  @Put()
+  async updateTask(@Body() input: UpdateTaskInput) {
+    return this.tasksService.updateTask(input);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: number) {
+    return this.tasksService.deleteTask(id);
+  }
+}
