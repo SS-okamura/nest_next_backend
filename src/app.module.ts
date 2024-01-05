046ac489f0f52b1e2239070f58c294cf66ca8e92
SS-okamura <ss.okamura0509@gmail.com>
2024-01-05 15:34:35 +0900

    feat: add tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './data-source';
 import { UsersModule } from './users/user.module';
+import { TaskModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    UsersModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
