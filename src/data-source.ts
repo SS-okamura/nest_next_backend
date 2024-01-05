2cbe604a1da48f6ea63918143e84f4385da2574b
SS-okamura <ss.okamura0509@gmail.com>
2024-01-05 15:49:34 +0900

    feat: dotenv

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,17 @@ import { Comment } from './entities/comment.entity';
 import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Task } from './entities/task.entity';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
 
 export const AppDataSource = new DataSource({
   type: 'mysql', // MySQL の場合
-  host: 'nest-next-my_sql-1', // docker-compose.yml で指定したコンテナの service 名
+  host: process.env.DB_HOST, // docker-compose.yml で指定したコンテナの service 名
   port: 3306, // ポート番号
-  username: 'user', // docker-compose.yml の MYSQL_USER
-  password: 'password', // docker-compose.yml の MYSQL_PASSWORD
-  database: 'sample', // docker-compose.yml の MYSQL_DATABASE
+  username: process.env.DB_USERNAME, // docker-compose.yml の MYSQL_USER
+  password: process.env.DB_PASSWORD, // docker-compose.yml の MYSQL_PASSWORD
+  database: process.env.DB_DATABASE, // docker-compose.yml の MYSQL_DATABASE
   logging: true, // コンソール画面に実行したSQLが表示される
   synchronize: false, // true にすると migration が自動で実行されます。
   entities: [Comment, User, Task], // エンティティクラスを指定する（複数の場合はカンマで区切る）
