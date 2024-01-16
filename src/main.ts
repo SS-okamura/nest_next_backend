331f357f845ea91b35cb9375120fbf4b3bc0e6ed
SS-okamura <ss.okamura0509@gmail.com>
2024-01-16 21:26:19 +0900

    feat: cors対策

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
