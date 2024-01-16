16e7bcf2ef127523663163c6e6a369633adbc00d
SS-okamura <ss.okamura0509@gmail.com>
2024-01-16 21:26:35 +0900

    feat: enumの追加

--- src/enums/taskStatus.ts
+++ src/enums/taskStatus.ts
@@ -10,3 +10,16 @@ export enum TaskStatus {
 export const defaultStatus = () => {
   return TaskStatus.waiting;
 };
+
+export const getTaskStatusFromString = (statusString: string) => {
+  // Enumの値を調べる
+  const statusValues = Object.values(TaskStatus) as string[];
+
+  // 文字列がEnumに存在するか確認
+  if (statusValues.includes(statusString)) {
+    return statusString as TaskStatus;
+  } else {
+    // 存在しない場合はundefinedを返す
+    return undefined;
+  }
+};
